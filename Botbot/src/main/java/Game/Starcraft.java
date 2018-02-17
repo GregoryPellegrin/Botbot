26c13b77ef40d56ed22bb267c91e62b669fb88a7
Gregory Pellegrin <Gregory Pellegrin@Hyperion.local>
2018-02-16 22:36:34 -0500

    Update

--- Botbot/src/main/java/Game/Starcraft.java
+++ Botbot/src/main/java/Game/Starcraft.java
@@ -4,6 +4,7 @@
  */
 package Game;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -14,7 +15,6 @@ import org.apache.http.util.EntityUtils;
 public class Starcraft
 {
 	/*
-	 * displayName,profilePath,
 	 * career(terranWins,protossWins,zergWins,highest1v1Rank)
 	 * swarmLevels(level,terran(level),zerg(level),protoss(level))
 	 * campaign(wol,hots)
@@ -26,11 +26,9 @@ public class Starcraft
 		{
 			HttpClient httpClient = HttpClientBuilder.create().build();
 			HttpResponse httpResponse = httpClient.execute(new HttpGet("https://" + regionName + ".api.battle.net/sc2/profile/" + profilId + "/1/" + playerName + "/?locale=en_US&apikey=tvx58hgpjg753jymgamk4q7pysrbrs5c"));
-			//Json.Starcraft starcraft = new Gson ().fromJson(EntityUtils.toString(httpResponse.getEntity()).replaceAll("null", "0"), Json.Starcraft.class);
-			System.out.println(EntityUtils.toString(httpResponse.getEntity()).replaceAll("null", "0"));
+			Json.Starcraft starcraft = new Gson ().fromJson(EntityUtils.toString(httpResponse.getEntity()).replaceAll("null", "0"), Json.Starcraft.class);
 			
-			return "";
-			//return starcraft.toString();
+			return starcraft.toString();
 		}
 		catch (IOException exception)
 		{

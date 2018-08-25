2a1757ec87543c44bec0566a31b8577363ef8465
Gregory Pellegrin <Gregory Pellegrin@DESKTOP-OERLAPI>
2018-08-25 18:02:31 -0400

    as

--- Botbot/src/main/java/key/ApiKey.java
+++ Botbot/src/main/java/key/ApiKey.java
@@ -2,7 +2,7 @@ package key;
 
 public class ApiKey
 {
-	public static final String DISCORD_TOKEN = "";
+	public static final String DISCORD_TOKEN = "t";
 	public static final String BLIZZARD_TOKEN = "";
 	public static final String BLIZZARD_API_KEY = "";
 }
